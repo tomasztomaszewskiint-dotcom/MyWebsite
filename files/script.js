1e6f3264133f572666c7727346945aa8bc736721
tomasztomaszewskiint-dotcom <tomasz.tomaszewski.int@gmail.com>
2026-07-23 20:48:18 +0200

    Delete files directory

--- files/script.js
+++
@@ -1,9 +0,0 @@
-const navToggle = document.getElementById('navToggle');
-const navLinks = document.getElementById('navLinks');
-
-if (navToggle && navLinks) {
-  navToggle.addEventListener('click', () => {
-    const isOpen = navLinks.classList.toggle('open');
-    navToggle.setAttribute('aria-expanded', String(isOpen));
-  });
-}
